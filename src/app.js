ca4d2fd124c55cdaaa68f5accbf832fafd20a78e
ttpunch <vinod418@gmail.com>
2024-12-07 22:13:32 +0530

    all working

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://drawing-frontend-ttpunch.vercel.app'],
+  origin: ['http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
