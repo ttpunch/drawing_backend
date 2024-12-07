011b28b3cf96fa8d1a1e63b3307b9483ba281343
ttpunch <vinod418@gmail.com>
2024-12-07 21:56:25 +0530

    fix: Update CORS configuration to allow frontend origins

--- src/app.js
+++ src/app.js
@@ -10,7 +10,12 @@ const rateLimit = require('express-rate-limit');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173', 'https://drawing-frontend-ttpunch.vercel.app'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
