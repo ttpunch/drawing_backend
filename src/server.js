61dae4f6c2d8a92d8c9f138d8595089a577c5084
ttpunch <vinod418@gmail.com>
2024-12-08 19:49:24 +0530

    for redder address

--- src/server.js
+++ src/server.js
@@ -8,9 +8,7 @@ const app = express();
 
 // Configure CORS
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production' 
-    ? process.env.FRONTEND_URL 
-    : 'http://localhost:5173',
+  origin: ['https://drawing-frontend.onrender.com', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
