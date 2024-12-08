07ee432e8e5360370b0547be4b0cc80427c55f6b
ttpunch <vinod418@gmail.com>
2024-12-08 22:32:45 +0530

    cors fixed

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://drawing-frontend.onrender.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
